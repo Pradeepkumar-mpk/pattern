23c361feff9e5f94a5d63c5ecfc8db49c2f132e6
Admin <admin@Tanveers-MacBook-Air.local>
2022-12-19 19:34:31 +0530

    adding square pattern program.

---
+++ square/main.js
@@ -0,0 +1,12 @@
+
+var row = parseInt(prompt("enter row limit :"));
+var column = parseInt(prompt("enter column limit :"));
+
+for(i=1;i<=row;i++)
+{
+	for(j=1;j<=column;j++)
+	{
+		document.write("*"+ "&nbsp &nbsp");
+	}
+	document.write("<br>");
+}
